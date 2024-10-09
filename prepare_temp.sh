cf1c6ad48a69b14efcb6fdf8bec446074c3da525
Charlie <charlie@ktty.au>
2024-10-10 03:51:35 +1030

    Add initial foundations

---
+++ prepare_temp.sh
@@ -0,0 +1,21 @@
+# TODO: implement this properly as apart of the setup script
+
+ansible_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+if ! ansible_exists ansible; then
+    echo "Ansible is not detected. Please install it to proceed."
+    exit 1
+fi
+
+if ! ansible_exists ansible-galaxy; then
+    echo "Ansible Galaxy is not detected. Please install it to proceed."
+    exit 1
+fi
+
+# install ansible roles
+ansible-galaxy role install geerlingguy.security geerlingguy.firewall geerlingguy.docker geerlingguy.pip
+
+# install ansible collections
+ansible-galaxy collection install community.general
